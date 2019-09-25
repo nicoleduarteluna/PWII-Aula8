8706d2117a44f35fdf0dcc99750374c8889abcb8
niluiz <nicoleduarteluna@hotmail.com>
2019-09-25 20:12:05 -0300

    Interpolation binding

---
+++ src/app/interpolation-binding/interpolation-binding.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'has-interpolation-binding',
+  templateUrl: './interpolation-binding.component.html',
+  styleUrls: ['./interpolation-binding.component.css']
+})
+export class InterpolationBindingComponent implements OnInit {
+
+    numero : number = 5; 
+
+    quadrado () : number {
+      return this.numero * this.numero;
+    }
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
